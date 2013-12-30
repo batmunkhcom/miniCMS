508e2cdddb2cfc21252d4c3f2bb8b4cd7ef8ec0b
BATMUNKH Moltov <contact@batmunkh.com>
2013-12-30 20:02:04 +0800

    setting component uusgelee

---
+++ core/components/setting/includes/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the miniCMS package.
+ * (c) since 2005 BATMUNKH Moltov <contact@batmunkh.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+function setting_save() {
+
+}
+
+function setting_form() {
+
+}
+
+/**
+ * Tohirgoog hadgalah function
+ * 
+ * @param string $setting_type Tohirgoonii turul
+ *
+ */
+function get_all_setting_value($setting_type = 'default') {
+    global $db;
+
+    $setting_db = new D\Model\Repository\UnitOfWork(
+            new D\Mapper\SettingMapper($db, new D\Model\Collection\EntityCollection), new D\Storage\ObjectStorage
+    );
+
+    $setting = new D\Model\Setting(array(
+        'name' => '',
+        'value' => '',
+        'type' => $setting_type
+    ));
+}
---
+++ core/components/setting/routing.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the miniCMS package.
+ * (c) since 2005 BATMUNKH Moltov <contact@batmunkh.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+set_route('setting_comment_action', '/setting/save');
+$router->with('/setting', function () use ($router) {
+
+    //comment home
+    $router->respond('GET', '/?', function ($request, $response) {
+
+    });
+
+    //comment save
+    $router->respond('POST', '/save', function ($request, $response, $service) {
+
+        //umnuh huudas ruu shidne
+        $service->back();
+    });
+
+    //comment update
+    $router->respond('GET', '/edit/[i:id]', function ($request, $response) {
+
+        set_get_parameter('id', $request->id);
+    });
+});
+
