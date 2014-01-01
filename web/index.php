4e1a3b767dc75642f89b7009924a891693b66a91
BATMUNKH Moltov <contact@batmunkh.com>
2014-01-01 14:44:13 +0800

    .

--- web/index.php
+++ web/index.php
@@ -8,21 +8,5 @@ require_once '../core/bootstrap.php';
 
 load_layout();
 
-load_component('test', array(
-    'code' => 'aaa'
-));
-
-$comp = $components;
-//print_r($_SESSION);
-echo '<hr>';
-echo get_route('form_comment_action');
-echo '..<hr>all--';
-print_r($comp->getAllComponents()->component_all);
-echo '..<hr>enabled---';
-print_r($comp->getAllEnabledComponents()->component_enabled);
-echo '..<hr>config ,enabled---';
-print_r(\M\Config::get('component_enabled'));
-echo '..<hr>';
-
 
 echo comment_form('test1', 'dd')->render();
