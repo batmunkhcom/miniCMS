55dcd68ddb7b996b6eae8bf3fc0a4630a2666a0d
Batmunkh Moltov <contact@batmunkh.com>
2014-01-03 08:37:15 +0800

    Category.php updated

--- core/library/Model/Category.php
+++ core/library/Model/Category.php
@@ -30,4 +30,34 @@ class Category extends D\Model\Category {
         return $all_categories;
     }
 
+	/**
+	* ID field eer songoh
+	* 
+	* @param integer $id ID field iin utga
+	* 
+	* @return object : Oldson object iig butsaana.
+	*/
+	public static function getById($id=0){
+		
+		global $db;
+
+		$mapper_db = db_unit($db,__CLASS__);
+	
+		$result = $mapper_db->fetchById($id);
+
+		return $result;
+	}
+
+	/**
+	* Ugugdsun field iin 1 utgiig butsaana.
+	* @param string $field_name : Filter leh fieldiin ner
+	* @param string $field_value : Filter hiih field iin utga
+	* @param string $to_get_field : Butsaah field iin ner
+	* 
+	* @return string $to_get_field iin utgiig butsaana
+	*/
+	public static function getByField($field_name,$field_value,$to_get_field){
+		
+	}
+
 }
