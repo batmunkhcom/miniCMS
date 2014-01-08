c5548036abc33678ed3125f2ac5275678e7a073e
Saruulbat <moon_mountain0111@yahoo.com>
2014-01-09 03:54:41 +0800

    left right function

---
+++ core/library/functions/left_right.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the miniCMS package.
+ * (c) since 2005 SARUULBAT Amarsaikhan <a.saruulbat@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Zuuniig avah
+ * 
+ * @param string $model Model iin ner
+ * @param array $fields Fielduud
+ * @param array $options Options 
+ */
+function get_left($model, $fields, $options=array()){
+ 
+    $left = 1;
+    
+    return $left;
+}
+
+/**
+ * Baruuniig avah
+ * 
+ * @param string $model Model iin ner
+ * @param array $fields Fielduud
+ * @param array $options Options 
+ */
+function get_right($model, $fields, $options=array()){
+ 
+    $right = 2;
+    
+    return $right;
+}
+
