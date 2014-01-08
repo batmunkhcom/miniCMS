1c7b5031804239eabbe0927b501e1824ef07706d
BATMUNKH Moltov <contact@batmunkh.com>
2014-01-09 03:09:32 +0800

    js/main, css/main -d function-d bsan header iig oruulav... header sent aldaa zaagaad bsan

--- core/library/functions/css.php
+++ core/library/functions/css.php
@@ -101,14 +101,9 @@ function compress_css() {
     header('Expires: ' . gmdate("D, d M Y H:i:s", time() + $offset) . ' GMT');
 
     if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $modified) {
-        header("HTTP/1.0 304 Not Modified");
-        header('Cache-Control:');
-    } else {
-        header('Cache-Control: max-age=' . $offset);
-        header('Content-type: text/css; charset=UTF-8');
-        header('Pragma:');
-        header("Last-Modified: " . gmdate("D, d M Y H:i:s", $modified) . " GMT");
 
+        //header iig main.php -d n oruulaad ugsun tul header iin medeelliig arilgav
+    } else {
 
         ob_start('ob_gzhandler');
 
--- web/js/main.php
+++ web/js/main.php
@@ -1,5 +1,19 @@
 <?php
 
+$offset = 60 * 60 * 24 * 7; // Cache for 1 weeks
+header('Expires: ' . gmdate("D, d M Y H:i:s", time() + $offset) . ' GMT');
+
+if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $modified) {
+    header("HTTP/1.0 304 Not Modified");
+    header('Cache-Control:');
+} else {
+    header("HTTP/1.0 200 OK");
+    header('Cache-Control: max-age=' . $offset);
+    header('Content-type: application/javascript; charset=UTF-8');
+    header('Pragma:');
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s", $modified) . " GMT");
+}
+
 define('APP_ENABLED', 'mbm');
 define('APPMODE', 'prod');
 
