4ca5063475d3ad982dfc1a3b7fba1a6bb5974327
BATMUNKH Moltov <contact@batmunkh.com>
2014-01-09 00:45:46 +0800

    Model zamchlal fixed

--- core/library/vendor/batmunkh/v3/src/D/Model/Search.php
+++ core/library/vendor/batmunkh/v3/src/D/Model/Search.php
@@ -7,7 +7,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace \D\Model;
+
+namespace D\Model;
 
 /**
  * Search model. search table.
