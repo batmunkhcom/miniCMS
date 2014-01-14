441351589f03b47827ae379491b09a29727b88d5
Saruulbat <moon_mountain0111@yahoo.com>
2014-01-14 20:24:10 +0800

    avatar

--- core/library/vendor/batmunkh/v3/src/F/Form/ContentForm.php
+++ core/library/vendor/batmunkh/v3/src/F/Form/ContentForm.php
@@ -34,20 +34,8 @@ class ContentForm extends \F\Form {
 //                ), array());
         //categories beld
         $categories = '
-            <!--multiple select start-->
-              <div class="row">
-              <div class="col-md-12">
-                  <section class="panel">
-                      <header class="panel-heading">
-                          Multiple Select
-                          <span class="tools pull-right">
-                            <a href="javascript:;" class="fa fa-chevron-down"></a>
-                            <a href="javascript:;" class="fa fa-times"></a>
-                          </span>
-                      </header>
-                      <div class="panel-body">
+            
                               <div class="form-group">
-                                  <label class="control-label col-md-3">Default</label>
                                   <div class="col-md-9">
                                       <select multiple="multiple" class="multi-select" id="categories" name="categories[]">
                                           ';
@@ -58,11 +46,7 @@ class ContentForm extends \F\Form {
                                       </select>
                                   </div>
                               </div>
-        </div>
-        </section>
-        </div>
-        </div>
-
+      
 
         ';
         $form->addElement(__('Select category2'), 'categories1[]', 'html', array(), array(), $categories);
