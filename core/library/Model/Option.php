4296a2055a6775a1c2f26a3a967a7827fd629b3c
Saruulbat <moon_mountain0111@yahoo.com>
2014-01-19 00:42:46 +0800

    Option list created

--- core/library/Model/Option.php
+++ core/library/Model/Option.php
@@ -1,11 +1,23 @@
 <?php
 
-/**
+/** 
  * This file is part of the miniCMS package.
  * (c) since 2005 SARUULBAT Amarsaikhan <a.saruulbat@gmail.com>
- *
+ * 
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-$template->set('is_admin', \M\Config::get('is_admin'));
-$template->set('options', \Option::fetchAll());
+
+class Option extends D\Model\Option {
+
+    public static function fetchAll() {
+
+        global $db;
+
+        $mapper_db = db_unit($db, __CLASS__);
+
+        $options = $mapper_db->fetchAll();
+        
+        return $options;
+    }
+}
