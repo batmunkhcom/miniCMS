042df54ba992f0fc41ee62c1c907758bd944b252
BATMUNKH Moltov <contact@batmunkh.com>
2014-01-20 16:21:28 +0800

    object insert done.

--- core/app/mbm/modules/object/action/admin_object_save.php
+++ core/app/mbm/modules/object/action/admin_object_save.php
@@ -48,21 +48,7 @@ if ($form->isValid('object')) {
 
 
     //content nemeh
-    //content left/right
-    if (post('parent_id') == 0) {
-        $lft = get_max_left('Content', post('parent_id')) + 2;
-        $rgt = $lft + 1;
-        $depth = 0;
-        $parent_id = 0;
-    } else {
-
-        $parent_content = \Content::fetchById(post('parent_id'));
-        $lft = ($parent_content->lft + 2);
-        $rgt = ($parent_content->lft + 3);
-        $depth = ($parent_content->depth + 1);
-        $parent_id = $parent_content->id;
-    }
-    //$object_db = db_mapper($db, 'Content');
+    //$object_db = db_mapper($db, 'Object');
     $object_db = new \D\Mapper\ObjectMapper($db, new \D\Model\Collection\EntityCollection);
     $object = new D\Model\Object(
             array(
@@ -90,12 +76,12 @@ if ($form->isValid('object')) {
     );
     $last_insert_id = $object_db->save($object);
 
-    //content category nemeh
+    //object category nemeh
     $c_category_db = db_unit($db, 'ObjectCategory');
     if (count(post('categories') > 0)) {
         foreach (post('categories') as $k => $v) {
             $c_category = new \D\Model\ObjectCategory(array(
-                'content_id' => $last_insert_id,
+                'object_id' => $last_insert_id,
                 'category_id' => $v
             ));
             $c_category_db->registerNew($c_category);
@@ -104,8 +90,24 @@ if ($form->isValid('object')) {
     }
     $c_category_db->commit();
 
+    //object options nemeh
+    $c_option_db = db_unit($db, 'OptionValue');
+    if (count(post('options') > 0)) {
+        foreach (post('options') as $k => $v) {
+            $c_option_value = new \D\Model\OptionValue(array(
+                'code' => 'object_' . $last_insert_id,
+                'option_id' => $k,
+                'option_value' => $v,
+                'is_active' => 1
+            ));
+            $c_option_db->registerNew($c_option_value);
+            unset($c_option_value);
+        }
+    }
+    $c_option_db->commit();
+
     set_flash(__('Object has been created'), 'success');
-    $session->clearKey('content');
+    $session->clearKey('object');
 
     header("Location: " . get_url('admin_object_list'));
 } else {
